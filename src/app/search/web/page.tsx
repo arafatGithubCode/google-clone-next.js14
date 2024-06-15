eeb50d8a8d7c47c6626547051bcae183da8a79e9
arafatGithubCode <md489288@gmail.com>
2024-06-15 20:58:32 +0600

    fetch the data from google search api and show the title in the web page

--- src/app/search/web/page.tsx
+++ src/app/search/web/page.tsx
@@ -1,5 +1,18 @@
-const SearchWeb = () => {
-  return <div>search web page</div>;
+type SearchParamsProps = {
+  searchParams: {
+    searchTerm: string;
+  };
+};
+const SearchWeb = async ({ searchParams }: SearchParamsProps) => {
+  const res = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.Go_Search_API_KEY}&cx=${process.env.CX_ENGINE_ID}&q=${searchParams.searchTerm}}`
+  );
+  const data = await res.json();
+  const results = data.items;
+
+  return (
+    <div>{results && results.map((result) => <h1>{result.title}</h1>)}</div>
+  );
 };
 
 export default SearchWeb;
