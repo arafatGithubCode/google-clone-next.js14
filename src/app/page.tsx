9af8f6753e6e39d36049db9cec3764b72e5206d8
arafatGithubCode <md489288@gmail.com>
2024-06-15 16:44:39 +0600

    add home header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
-import Image from "next/image";
+import HomeHeader from "@/components/header/HomeHeader";
 
 export default function Home() {
-  return <main className="text-red-500">app</main>;
+  return (
+    <>
+      <HomeHeader />
+    </>
+  );
 }
