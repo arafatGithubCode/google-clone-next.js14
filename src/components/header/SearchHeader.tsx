acd01a3189b64e288977738ccd566b98bbbf6457
arafatGithubCode <md489288@gmail.com>
2024-06-15 20:23:05 +0600

    add search options

--- src/components/header/SearchHeader.tsx
+++ src/components/header/SearchHeader.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import SearchBox from "./SearchBox";
 import { RiSettings3Line } from "react-icons/ri";
 import { TbGridDots } from "react-icons/tb";
+import SearchHeaderOptions from "./SearchHeaderOptions";
 
 const SearchHeader = () => {
   return (
@@ -29,6 +30,7 @@ const SearchHeader = () => {
           Sign in
         </button>
       </div>
+      <SearchHeaderOptions />
     </header>
   );
 };
